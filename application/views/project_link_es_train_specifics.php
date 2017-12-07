31d0ae200677123432bf47072b7aad81d8df2b4a
jerem1508 <jeremy.peglion@gmail.com>
2017-12-07 17:37:35 +0100

    correction couleur + bug 404 jquery.awesome-cursor.min.js

--- application/views/project_link_es_train_specifics.php
+++ application/views/project_link_es_train_specifics.php
@@ -5,5 +5,3 @@
 <script type="text/javascript" src="<?php echo base_url('assets/bootstrap-tagsinput.min.js');?>"></script>
 
 <script type="text/javascript" src="<?php echo base_url('assets/project_link_es_train.js');?>"></script>
-
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/socket.io/1.3.6/socket.io.min.js"></script>
