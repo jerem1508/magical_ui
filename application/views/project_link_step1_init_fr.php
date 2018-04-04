ba7afccaa0ef9ef3f0ee7b3381bfdc19b24f4229
Jerem <jeremy.peglion@gmail.com>
2018-04-04 14:04:52 +0200

    MAJ CGU

--- application/views/project_link_step1_init_fr.php
+++ application/views/project_link_step1_init_fr.php
@@ -414,21 +414,61 @@ function get_normalized_projects_html($id, $normalized_projects)
                             <div class="row">
 
                                 <div class="col-md-offset-1 col-md-10" id="txt_cgu" style="overflow-y: auto;height: 400px;">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                    <br /><br />
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                    <br /><br />
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                    <br /><br />
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                    <br /><br />
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                    <br /><br />
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                    <br /><br />
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    Tout utilisateur de la plateforme reconnaît expressément souscrire sans réserve aux présentes conditions générales d’utilisation (CGU). L’utilisation de la plateforme suppose le respect total des lois, règlements et tous les autres textes juridiques en vigueur. Il conviendra de respecter l’ensemble de ces dispositions et règles en toutes circonstances. À défaut, l’utilisateur est conscient qu’il risque des sanctions civiles, pénales et/ou disciplinaires.
+
+                            		<h4>Éditeur de "La machine à données"</h4>
+
+                            		"La machine à données" est un service proposé de manière ouverte et gratuite par le <a href="http://www.enseignementsup-recherche.gouv.fr/">ministère de l’enseignement supérieur, de la recherche et de l’innovation</a> (MESRI). Le Ministère de l’enseignement supérieur, de la recherche et de l’innovation est l’éditeur de ce service.
+
+                            		<h4>Disponibilité du site</h4>
+
+                            		L’éditeur s’engage à mettre en œuvre tout ce qui est possible techniquement pour sécuriser l’accès et l’utilisation du service "La machine à données". Il est accessible 24 heures sur 24, 7 jours sur 7, sauf en cas de force majeure ou de survenance d’un événement hors du contrôle de l’éditeur et sous réserve d’éventuelles pannes et interventions de maintenance nécessaires au bon fonctionnement du service.
+
+                            		L’éditeur ne garantit pas que le service fonctionne de manière ininterrompue, sécurisée, qu’il soit exempt d’erreurs et que son niveau de performance soit homogène d’un jeu de données à l’autre. Les interventions de maintenance pourront être effectuées sans que les utilisateurs de "La machine à données" n’aient été préalablement avertis.
+
+                            		L’éditeur ne peut être tenu pour responsable d'une éventuelle rupture de ce service ou d'un problème technique empêchant un utilisateur d'accéder au service "La machine à données".
+
+                            		L’éditeur se réserve la possibilité de refuser l'accès aux services offerts par "La machine à données" en cas d'usage abusif ou de violation de dispositions législatives ou réglementaires en vigueur.
+
+                            		<h4>Données personnelles</h4>
+
+                            		En application de la loi n°78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, les utilisateurs du service "La machine à données" disposent notamment d’un droit d’accès et de rectification auprès de l’éditeur de la plateforme.
+
+                            		Ce droit s'exerce auprès du Ministère de l’enseignement supérieur, de la recherche et de l’innovation, service de la coordination des stratégies de l’enseignement supérieur et de la recherche, sous-direction des systèmes d’information et des études statistiques, département des outils d’aide à la décision, 1 rue Descartes, 75005 Paris (ou à l’adresse de contact XXXXX).
+
+                            		Le service "La machine à données" a été déclaré à la Commission Nationale de l'Informatique et des Libertés sous le numéro : XXXXXXXX. Pour se désinscrire de "La machine à données" (utilisateur identifié) et supprimer données personnelles et jeux de données associés, consulter XXXX
+
+                            		Les données personnelles des utilisateurs ne sont ni cédées ni transmises.
+
+                            		L’éditeur se réserve le droit, à la seule fin d’améliorer la qualité du service de contacter par mail ses utilisateurs. L’éditeur s’engage à faire un usage modéré de ce droit.
+
+                            		<h4>Utilisateur identifié</h4>
+
+                            		Pour bénéficier de l’ensemble des services de "La machine à données", les utilisateurs doivent créer un compte utilisateur. Les comptes utilisateurs sont personnels et l’accès à un même compte ne peut être partagé par plusieurs utilisateurs. À défaut, sans aucun préavis, l’éditeur se réserve le droit d’interdire l’accès au service à des utilisateurs ne respectant pas cette règle et de supprimer l’ensemble des informations associées (y compris les jeux de données associés à ce compte).
+
+                            		<h4>Format des jeux de données acceptés par le service</h4>
+
+                            		"La machine à données" accepte les données tabulaires répondant uniquement aux formats suivants : csv, xls et xlsx.
+
+                            		<h4>Stockage des données soumises au service</h4>
+
+                            		Les données sont supprimées à l’issue de la session d’utilisation du service "La machine à données". Les données soumises au service "La machine à données" (via l’interface web ou API) par les utilisateurs en mode identifié sont stockées, si l’utilisateur le souhaite, à des fins de production ultérieure ou récurrente d’un même traitement. En mode identifié, les utilisateurs peuvent donner leur consentement pour permettre à l’éditeur d’utiliser les données transmises à "La machine à données" à des fins d’amélioration de la qualité du service.
+
+                            		Entre le 1er et le 5 de chaque mois, sans préavis, l’éditeur procède à la suppression des jeux de données n’ayant pas fait l’objet d’un usage par le service "La machine à données".
+
+                            		<h4>Code de "La machine à données"</h4>
+
+                            		L’intégralité du code de la plate-forme est gratuitement disponible en Open source sur <a href="https://github.com" target="_blank">Github</a> sous le nom de projet <a href="https://github.com/entrepreneur-interet-general/Merge-Machine" target="_blank">Merge-Machine</a> sous licence <a href="https://opensource.org/licenses/MIT" target="_blank">MIT</a>.
+
+                            		Vous pouvez contribuer en suivant la documentation dédiée et en proposant des pull-requests.
+
+                            		<h4>Usage équitable du service</h4>
+
+                            		Seul un usage équitable des services "La machine à données" (tant via l’interface web que via API) est autorisé. C’est-à-dire qu’un même utilisateur ne doit pas, par un usage trop
+
+                            		intensif du service, dégrader de manière sensible la qualité du service proposée à tous les autres. L’éditeur pourra, sans préavis, interrompre un usage irrespectueux de ce principe..
+
+                            		Les utilisateurs désireux d’effectuer un usage dépassant les ressources affectées à "La machine à données" par l’éditeur pourra déployer un service équivalent dans un environnement sous leur contrôle et leur entière responsabilité (voir « Code de "La machine à données" »).
                                 </div>
                             </div>
                         </div>
@@ -1241,7 +1281,7 @@ function get_normalized_projects_html($id, $normalized_projects)
 
         // Scroll CGU
         $("#txt_cgu").scroll(function() {
-           if($("#txt_cgu").scrollTop() >= 260) {
+           if($("#txt_cgu").scrollTop() >= 500) {
                $("#chk_cgu").prop("disabled", false);
                $("#label_cgu").removeClass("chk_label_disabled");
                $("#label_cgu").addClass("chk_label_enabled");
